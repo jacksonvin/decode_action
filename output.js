37e43b310b0f6a0d2ab674fdd503a7b2455670e7
action <action@github.com>
2025-09-23 14:48:11 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Tue Sep 23 2025 14:47:41 GMT+0000 (Coordinated Universal Time)
+//Tue Sep 23 2025 14:48:11 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 LOTTERY = 1;
